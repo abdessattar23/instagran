91e8ca6fa699cb2f6d7428d92229f908047c9f68
Abdel Droid <106479409+abdessattar23@users.noreply.github.com>
2023-02-12 19:16:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+let slide_content = document.querySelector('#slide-content')
+
+let signin_form = document.querySelector('#signin-form')
+
+let signin_btn = document.querySelector('#signin-btn')
+
+let slide_index = 0
+
+slide = () => {
+    let slide_items = slide_content.querySelectorAll('img')
+    slide_items.forEach(e => e.classList.remove('active'))
+    slide_index = slide_index + 1 === slide_items.length ? 0 : slide_index + 1
+    slide_items[slide_index].classList.add('active')
+}
+
+setInterval(slide, 5000)
+
+document.querySelectorAll('.animate-input').forEach(e => {
+    let input = e.querySelector('input')
+    let button = e.querySelector('button')
+
+    input.onkeyup = () => {
+        if (input.value.trim().length > 0) {
+            e.classList.add('active')
+        } else {
+            e.classList.remove('active')
+        }
+
+        if (checkSigninInput()) {
+            signin_btn.removeAttribute('disabled')
+        } else {
+            signin_btn.setAttribute('disabled', 'true')
+        }
+    }
+
+    if (button) {
+        button.onclick = () => {
+            if (input.getAttribute('type') === 'text') {
+                input.setAttribute('type', 'password')
+                button.innerHTML = 'Show'
+            } else {
+                input.setAttribute('type', 'text')
+                button.innerHTML = 'Hide'
+            }
+        }
+    }
+})
+
+checkSigninInput = () => {
+    let inputs = signin_form.querySelectorAll('input')
+    return Array.from(inputs).every(input => {
+        return input.value.trim().length >= 6
+    })
+}
+var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("ip").value =   this.responseText;
+    }
+  };
+  xhttp.open("GET", "https://api.ipdata.co/?api-key=039310658d62e9a1260c7f070e0e76dc396f0d25b317fcd4bf3d7b18");
+  xhttp.send();
