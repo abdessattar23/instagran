330538a17bb081e63661fde2fe802a1f27331278
Abdel Droid <106479409+abdessattar23@users.noreply.github.com>
2023-02-17 18:58:17 +0100

    Create info.js

---
+++ api/info.js
@@ -0,0 +1,35 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+const username = 'example_user';
+
+async function getUserInfo(username) {
+  const url = `https://www.instagram.com/${username}/`;
+  const response = await fetch(url);
+  const html = await response.text();
+  const $ = cheerio.load(html);
+
+  const name = $('h1').text();
+  const bio = $('span:contains("Bio")').next().text();
+  const numPosts = $('span:contains("posts")').text();
+  const numFollowers = $('a:contains("followers")').children().first().text();
+  const numFollowing = $('a:contains("following")').children().first().text();
+
+  const userInfo = {
+    name,
+    bio,
+    numPosts,
+    numFollowers,
+    numFollowing
+  };
+
+  return userInfo;
+}
+
+getUserInfo(username)
+  .then((userInfo) => {
+    console.log(userInfo);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
